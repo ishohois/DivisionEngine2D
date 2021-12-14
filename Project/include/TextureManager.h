ce15e8b71629c54094e00d3a9d981148ba98376e
Egil-boop <82576944+Egil-boop@users.noreply.github.com>
2021-12-14 15:48:39 +0100

    Börjat kommentera delar av kod
    Texture.h/cpp har jag börjat kommentera för att man ska få större förståelse för vad som händer

--- Project/include/TextureManager.h
+++ Project/include/TextureManager.h
@@ -6,15 +6,32 @@
 namespace diva
 {
 
+    /*What does this class do?
+        This class implements a design pattern "sigelton" the reson behinde this is that we only 
+        want one instance of this class to be used at anytime.
+    
+        This class is used for load/drawing out our differnet objects to the screen. It uses pos of the object and alos it's 
+        with and height to determent it's pos on the screen.
+
+        The load function takes a filepath and gives each object to draw an ID. We want this becuase it's 
+        a way to know what item we want to work with and we can use the ID to find the pos in the map that we use to
+        store all the textures. In the drawFrame function we alos take in a row and a frame 
+        this eneble us to do simple animation from sprtiessheets. More infromation on what they do in this class CPP.
+        
+     */
+
     class TextureManager
     {
     private:
-        TextureManager()= default;
-        static TextureManager* instance; 
+        // Implementing a Singelton.
+        TextureManager() = default;
+        static TextureManager *instance; // A pointer to a static instance.
     public:
-
-        static TextureManager * getInstance(){
-            if(!instance){
+        static TextureManager *getInstance()
+        {   // this is how we accses an instance.
+            // If there are no instance then we create one else we just retern the one that exist.
+            if (!instance)
+            {
                 instance = new TextureManager();
             }
             return instance;
@@ -30,9 +47,8 @@ namespace diva
         void drawFrame(std::string id, int x, int y, int widht, int height, int currentRow, int currentFrame,
                        SDL_Renderer *TRenderer, SDL_RendererFlip = SDL_FLIP_NONE); // dose the same as draw but adds a chceck
                                                                                    // what row we are on in the image spredsheet and also what frame we are in at the moment.
-
-        std::map<std::string, SDL_Texture*> TTexturemap;
-
+        // This map will contain all the textures in the game.
+        std::map<std::string, SDL_Texture *> TTexturemap;
     };
 
 }
--- Project/src/TextureManger.cpp
+++ Project/src/TextureManger.cpp
@@ -2,19 +2,20 @@
 #include <SDL2/SDL_image.h>
 namespace diva
 {
-    TextureManager *TextureManager::instance = nullptr;
-    // When we call this funtion we have a texture that can be used by accessing it form it's ID.
-    // Useing this in draw;
+    TextureManager *TextureManager::instance = nullptr; // inizilasing the instance.
+ 
     bool TextureManager::load(std::string fileName, std::string id, SDL_Renderer *TRenderer)
     {
-        SDL_Surface *TTempsurface = IMG_Load(fileName.c_str());
-
+        SDL_Surface *TTempsurface = IMG_Load(fileName.c_str()); // Loading in the texture to a surface using the file name from the function call.
+        // checking if we could load in the file.
         if (TTempsurface == 0)
         {
+            std::cout << "File not loaded in to the surface. FilePath incorrect?" << std::endl;
             return false;
         }
-
+        // creates the textrure from the surface and puts it in a render context.
         SDL_Texture *TTexture = SDL_CreateTextureFromSurface(TRenderer, TTempsurface);
+        // freeing up the surface cause it's no loger needed.
         SDL_FreeSurface(TTempsurface);
 
         if (TTexture == 0)
@@ -23,14 +24,17 @@ namespace diva
             return false;
         }
 
-        // If everythiing loaded, add the texture to the list
+        // If everythiing loaded, add the texture to the map, using the texture ID as a key.
         TTexturemap[id] = TTexture;
         return true;
     }
-
+    // a fucntion to draw the texture on the screen. 
     void TextureManager::draw(std::string id, int x, int y, int width, int height, SDL_Renderer *TRenderer, SDL_RendererFlip flip)
     {
-        SDL_Rect srcRect;
+        // The srcRect copys the part of the image we want to from the texture to the screen. We use it to set the pos of the texture(sprite) 
+        // and we alos use it to define the width and height of the pcitre to display. 
+        SDL_Rect srcRect; 
+        // we use destRect to set the new pos of the srcRect. 
         SDL_Rect destRect;
 
         srcRect.x = 0;
@@ -50,8 +54,9 @@ namespace diva
       
         SDL_Rect srcRect;
         SDL_Rect destRect;
-
+        // here we use the current frame to determent what part of the spritesheet to display.
         srcRect.x = width * currentFrame;
+        
         srcRect.y = height * (currentRow - 1);
         srcRect.w = destRect.w = width;
         srcRect.h = destRect.h = height;
