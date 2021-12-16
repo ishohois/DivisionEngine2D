306d909f19dc56945223dad3c19d8fefb7c90c3b
Agzzel <sterneraxel@protonmail.com>
2021-12-16 09:28:15 +0100

    Skapat headers för button, label och uiobject

--- Project/include/Button.h
+++ Project/include/Button.h
@@ -1,17 +1,18 @@
-#ifndef UIOBJECT_H
-#define UIOBJECT_H
-#include "GameObject.h"
-#include <SDL2/SDL.h>
+#ifndef BUTTON_H
+#define BUTTON_H
+#include "UIObject.h"
+#include <string>
+#include "SDL2/SDL.h"
 
-namespace diva {
-    class UIObject : public GameObject{
+namespace diva{
+    class Button : public UIObject{
         public:
         virtual void mouseDown(int x, int y){}; 
         virtual void mouseUp(int x, int y){};
         void setText(std::string input){};
         std::string getText(){};
+        SDL_Rect rectCollider;
     };
 }
-
 
 #endif
---
+++ Project/include/Label.h
@@ -0,0 +1,14 @@
+#ifndef LABEL_H
+#define LABEL_H
+#include "UIObject.h"
+#include "SDL2/SDL.h"
+#include <string>
+
+namespace diva
+{
+    class Label : public UIObject{
+        public: 
+        void setText(std::string input){};
+        std::string getText(){};
+    };
+} 
